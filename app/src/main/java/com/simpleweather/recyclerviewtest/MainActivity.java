9de6008fafe04a1a574712430bb48c03350037f4
LICONION <969899993@qq.com>
2017-04-01 00:17:23 +0800

    first commit

---
+++ app/src/main/java/com/simpleweather/recyclerviewtest/MainActivity.java
@@ -0,0 +1,81 @@
+package com.simpleweather.recyclerviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private List<String> mDatas;
+    private RecyclerView recyclerView;
+    private MyAdapter myAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initData();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayout.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        myAdapter = new MyAdapter();
+        recyclerView.setAdapter(myAdapter);
+
+
+    }
+
+    private void initData() {
+        mDatas = new ArrayList<String>();
+        for (int i = 'A'; i < 'z'; i++) {
+            mDatas.add("" + (char) i);
+        }
+    }
+
+    private class MyAdapter extends RecyclerView.Adapter {
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(getApplicationContext())
+                    .inflate(R.layout.item, parent, false);
+
+            MyViewHolder myViewHolder = new MyViewHolder(view);
+            return myViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            MyViewHolder myViewHolder = (MyViewHolder) holder;
+            myViewHolder.setData(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mDatas.size();
+        }
+    }
+
+    private class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView tv;
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            tv = (TextView) itemView.findViewById(R.id.tv);
+        }
+
+
+        public void setData(int position) {
+            tv.setText(mDatas.get(position));
+        }
+    }
+}
